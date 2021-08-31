e6141627326c9bb86cccbc72928fc9b73541e62f
anotherSimpleCoder <71701743+anotherSimpleCoder@users.noreply.github.com>
2021-08-31 21:21:26 +0200

    its getting there..

--- executer/include/executer.h
+++ executer/include/executer.h
@@ -1,12 +1,17 @@
 #include <vector>
+#include <unordered_map>
+#include <string>
 #include "asr.h"
 #include "fileReader.h"
 
+
 struct executer {
 	asr* asrInstance;
+	std::unordered_map<std::string, int> variables;
 };
 
 typedef struct executer executer;
 
 void printRegister(std::vector<entry> r);
 void execute(executer* e, std::vector<entry> r);
+string constructString(std::vector<uint16_t> chars, int startPosition, int* endPosition);
--- executer/src/executer.cpp
+++ executer/src/executer.cpp
@@ -4,6 +4,7 @@
 
 #include "executer.h"
 
+
 using namespace std;
 
 void execute(executer* e, std::vector<entry> r) {
@@ -31,7 +32,102 @@ void execute(executer* e, std::vector<entry> r) {
 
 		e->asrInstance = new asr(width, height);
 		cout << title << endl;	
+
+		string varName = "";
+		int varValue = 0;
+		int end = 0;
+		int contentSize = 0;
+
+		int x, y, red, green, blue, alpha = 0;
+		string nameBuf = "";
+		int amount = 0;
+
+		if(r[4].content[0] == SEC_BODY) {
+			for(int i = 5; i < size; i++) {
+				e_buf = r[i];
+				
+				switch(e_buf.type) {
+					case entryType::ASGN:
+						varName = "";
+						end = 0;
+						
+						contentSize = e_buf.content.size();
+
+						if(e_buf.content[0] == ALPHABETIC) {
+							varName = constructString(e_buf.content, 1, &end);
+						}
+
+						varValue = e_buf.content[end + 1];
+						
+						if(e->variables.find(varName) == e->variables.end()){
+							e->variables.insert(std::pair<string, int>(varName, varValue));
+						}
+
+						else {
+							e->variables[varName] = varValue;
+						}
+						cout << varName << ":" << e->variables[varName] << endl;
+						break;
+
+
+					case entryType::COM:
+						x = 0;
+						y = 0;
+
+						red = 0;
+						green = 0;
+						blue = 0;
+						alpha = 0;
+
+						contentSize = e_buf.content.size();
+						
+						nameBuf = "";
+						end = 0;
+						amount = 0;						
+
+						for(int i = 0; i < size; i++) {
+							if(e_buf.content[i] == ALPHABETIC) {
+								i++;
+								nameBuf = constructString(e_buf.content, i, &end);
+								i = end;
+								cout << << nameBuf << endl;
+								//get val and store
+								amount++;
+							}
+
+							else {
+								//get val and store
+								cout << (int)e_buf.content[i] << endl;
+								amount++;
+							}
+						}
+
+						cout << amount << endl;
+						break;
+
+
+					default:
+						break;	
+				}	
+			}
+		}
 	}
+}
+
+
+string constructString(std::vector<uint16_t> chars, int startPosition, int* endPosition) {
+	int d = startPosition;
+	string res = "";
+
+	while(chars[d] != ALPHABETIC) {
+		char c = (char)chars[d];
+		res += c;
+		d++;
+	}
+
+	*endPosition = d;
+
+	return res;
 }
 
 void printRegister(std::vector<entry> r) {
